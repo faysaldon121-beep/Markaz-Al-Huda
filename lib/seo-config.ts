6f718320e775054317dd77896470361527114873
faysaldon121-beep <faysaldon121@gmail.com>
2026-03-23 02:53:46 +0500

    Update baseUrl in SEO_CONFIG

---
+++ lib/seo-config.ts
@@ -0,0 +1,32 @@
+export const SEO_CONFIG = {
+  baseUrl: 'https://markazulhuda.vercel.app',
+  siteName: 'Markaz ul Huda',
+  siteTitle: 'Markaz ul Huda | Quran School Kot Abdullah Qasoor',
+  description: 'Islamic school offering Hifz, Tajweed, Manzil, and Maariful Quran programs.',
+  locale: 'en_PK',
+
+  phone: '+92-344-1722419',
+  email: 'info@markazulhuda.pk',
+  address: 'Kot Abdullah, Qasoor, Punjab, Pakistan',
+  
+  defaultKeywords: [
+    'Quran school Pakistan',
+    'Hifz Quran Qasoor',
+    'Islamic school Kot Abdullah',
+    'Tajweed classes',
+    'online Quran courses',
+    'Maariful Quran',
+  ],
+
+  ogImage: '/og-image.jpg',
+  twitterHandle: '@markazulhuda',
+
+  socialMedia: {
+    facebook: 'https://facebook.com/markazulhuda',
+    twitter: 'https://twitter.com/markazulhuda',
+    instagram: 'https://instagram.com/markazulhuda',
+    youtube: 'https://youtube.com/@markazulhuda',
+  },
+
+  googleSiteVerification: 'Oxz_vV17FHU_g0ColbVp2jZ2ANwgg1udZlvwTRMrqgI',
+};
